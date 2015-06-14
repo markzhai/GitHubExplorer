ec3064d50ade31167b2e407f726f69e16b31b5b7
markzhai <zhaiyifan56@gmail.com>
2015-06-14 13:05:11 +0800

    update icon and app name, add RoboGuice 3.0

---
+++ app/src/main/java/cn/zhaiyifan/github/ui/BaseActivity.java
@@ -0,0 +1,9 @@
+package cn.zhaiyifan.github.ui;
+
+import roboguice.activity.RoboActionBarActivity;
+
+/**
+ * Created by markzhai on 2015/6/14.
+ */
+public class BaseActivity extends RoboActionBarActivity {
+}
--- app/src/main/java/cn/zhaiyifan/github/ui/MainActivity.java
+++ app/src/main/java/cn/zhaiyifan/github/ui/MainActivity.java
@@ -1,10 +1,10 @@
 package cn.zhaiyifan.github.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,21 +12,39 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import cn.zhaiyifan.github.R;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
 
-public class MainActivity extends AppCompatActivity {
+    @InjectView(R.id.hello)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         RequestQueue mQueue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest("https://github.com/trending",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("TAG", response);
+                        Document doc = Jsoup.parse(response);
+                        Element languageFilterListElement = doc.select(".language-filter-list").first();
+                        languageFilterListElement.select(".selected").first();
+
+                        // language list link and language text
+                        for (Element languageElement : languageFilterListElement.children()) {
+                            String linkHref = languageElement.getElementsByTag("a").attr("href");
+                            String linkText = languageElement.text();
+                            Log.d("TAG", languageElement.toString());
+                            textView.setText(linkText);
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
